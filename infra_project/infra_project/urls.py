8b0b412de2d6191402e39d01e514a00b3ad04d2e
Роман Кислов <roman@kislovs.ru>
2022-08-31 16:55:52 +0500

    test9

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import include, path
 from django.contrib import admin
 
+
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
